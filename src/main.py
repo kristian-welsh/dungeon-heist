474284e78c5a0a42d7d65c825724139983a21722
Kristian Welsh <kriswelsh@gmail.com>
2020-12-11 22:53:22 +0000

    seperate world gen and world display
    at some point i'll want to generate the world once
    and display the world many times as it changes
    Introduction of Cell objects allows game objects to
    have more use than simple display, so solidity, visibility, weight, etc

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
+class Cell:
+    def __str__(self):
+        return "null_cell"
+
+class Ground(Cell):
+    def __str__(self):
+        return "."
+
 class Grid:
     def display(self, x, y):
-        return "."
+        return str(self.cells[y][x])
 
     def __str__(self):
         output = ""
@@ -13,6 +21,14 @@ class Grid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.generate_ground()
+
+    def generate_ground(self):
+        self.cells = []
+        for y in range(self.height):
+            self.cells.append([])
+            for x in range(self.width):
+                self.cells[y].append(Ground())
 
 def print_grid():
     height = 25
