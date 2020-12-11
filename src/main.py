95bfb4b88ce783314d43cb23eecf695452a50a0d
Kristian Welsh <kriswelsh@gmail.com>
2020-12-11 22:38:01 +0000

    refactor behaviour to grid object

--- src/main.py
+++ src/main.py
@@ -2,19 +2,22 @@ class Grid:
     def display(self, x, y):
         return "."
 
-def generate_grid():
-    return Grid()
+    def __str__(self):
+        output = ""
+        for y in range(self.height):
+            for x in range(self.width):
+                output += self.display(x, y)
+            output += "\n"
+        return output
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 def print_grid():
-    grid = generate_grid()
-    output = ""
     height = 25
     width = 50
-    for y in range(height):
-        for x in range(width):
-            output += grid.display(x, y)
-        output += "\n"
-    print(output)
+    print(Grid(width, height))
 
 print_grid()
 
