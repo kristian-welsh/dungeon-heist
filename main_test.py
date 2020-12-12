c49ff25c6315e97cb7c919a430997e41b7249fee
Kristian Welsh <kriswelsh@gmail.com>
2020-12-12 23:24:23 +0000

    basic test setup

---
+++ main_test.py
@@ -0,0 +1,11 @@
+import unittest
+import main
+
+class TestDungeon(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(True)
+
+class Utils(unittest.TestCase):
+    def test_clone_grid(self):
+        self.assertTrue(True)
+
