3857bfa0907b413deef29a84772aac5b6dc98900
Kristian Welsh <kriswelsh@gmail.com>
2020-12-21 21:43:24 +0000

    fully test player constructor

--- src/test/test_player.py
+++ src/test/test_player.py
@@ -1,12 +1,23 @@
 import unittest
 import mock
 
+from src.main.cells import PlayerCell
 from src.main.player import Player
 
 class TestPlayer(unittest.TestCase):
-    @mock.patch('src.main.grid.Grid')
-    def test_retains_position(self, grid_mock):
+    def test_retains_position(self):
         foo = Player(3, 4)
-        print(grid_mock)
         self.assertEqual(3, foo.x)
         self.assertEqual(4, foo.y)
+
+    def test_can_update_player_position(self):
+        foo = Player(1, 1)
+        foo.update(2, 3)
+        self.assertEqual(foo.x, 3)
+        self.assertEqual(foo.y, 4)
+
+    @mock.patch('src.main.player.Grid')
+    def test_retains_grid(self, grid_mock):
+        foo = Player(0, 0)
+        grid_mock.assert_called()
+                                                               
