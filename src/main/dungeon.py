39442d2184c6ddb6a5f3da16add1f3ce5dc00f81
Kristian Welsh <kriswelsh@gmail.com>
2020-12-23 19:39:43 +0000

    implement collision with walls

--- src/main/dungeon.py
+++ src/main/dungeon.py
@@ -6,24 +6,42 @@ class Dungeon:
     """ todo: extract world shard base class
         create method to stamp on on top of other
         params x and y starting point
-        dont go over edge of bottom shard """
+        dont go over edge of bottom shard
+        
+        this class will evolve to become the logic for actions
+        to perform in the dungeon, manipulated by game.
+        There will be a seperate object for generating cells, 
+        and a constructor here will accept a grid.
+        Think of it like Dungeon is the dungeon as it currently
+        exists, inclusive of changes and current state. """
 
     def __str__(self):
-        grid = self.grid
-        for room, x, y in self.rooms:
-            grid = grid.add_grids(room, x, y)
-        grid = grid.add_grids(self.player.grid, self.player.x, self.player.y)
-        return str(grid)
+        grid, x, y = (self.player.grid, self.player.x, self.player.y)
+        display = self.grid.add_grids(grid, x, y)
+        return str(display)
 
     def __init__(self, width, height):
-        self.grid = Grid(width, height, lambda:Wall())
+        self.player = Player(6, 7)
         self.rooms = [
             (Grid(10, 5, lambda:Ground()), 3, 6),
             (Grid(6, 9, lambda:Ground()), 20, 5),
-            (Grid(11, 3, lambda:Ground()), 27, 13)
+            (Grid(11, 3, lambda:Ground()), 27, 13),
         ]
-        self.player = Player(6, 7)
+        self.grid = self.make_grid(width, height)
+
+    def make_grid(self, width, height):
+        grid = Grid(width, height, lambda:Wall())
+        for room, x, y in self.rooms:
+            grid = grid.add_grids(room, x, y)
+        return grid
+
 
     def update_player(self, addx, addy):
-        self.player.update(addx, addy)
+        if not self.collide(addx, addy):
+            self.player.update(addx, addy)
+
+    def collide(self, addx, addy):
+        new_x = self.player.x + addx
+        new_y = self.player.y + addy
+        return self.grid.cells[new_y][new_x].collides()
 
--- src/test/test_dungeon.py
+++ src/test/test_dungeon.py
@@ -1,8 +1,31 @@
 import unittest
 from src.main.dungeon import Dungeon
+from src.main.grid import Grid
+from src.main.cells import Wall
 
 class TestDungeon(unittest.TestCase):
     def test_init(self):
         # todo
+        # behaviour tested here will later be extracted to WorldGen
         self.assertTrue(True)
+
+    def test_collision_doesnt_occur(self):
+        dungeon = Dungeon(50, 25)
+        player = dungeon.player
+        player.x = 6
+        player.y = 7
+
+        result = dungeon.collide(1, 0)
+        dungeon.update_player(1, 0)
+
+        self.assertFalse(result)
+        self.assertEqual(7, player.x)
+        self.assertEqual(7, player.y)
+
+    def test_collision_occurs(self):
+        """test correctness assumes placement of player in room by dungeon"""
+        dungeon = Dungeon(50, 25)
+        dungeon.grid.cells[7][7] = Wall()
+        result = dungeon.collide(1, 0)
+        self.assertTrue(result)
 
