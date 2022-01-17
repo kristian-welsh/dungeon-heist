963f45f4a26f405a224a9d82c6688a731b129cc7
Kristian Welsh <kriswelsh@gmail.com>
2022-01-17 18:25:58 +0000

    refactor grid merging in dungeon, add test room to house player

--- src/main/dungeon.py
+++ src/main/dungeon.py
@@ -13,27 +13,31 @@ class Dungeon:
         exists, inclusive of changes and current state. """
 
     def __str__(self):
-        display = self.grid.add_grids(self.player.grid)
+        display = self.grid
+        display = display.add_grids(self.player.grid)
         return str(display)
 
     def __init__(self, width, height, room_generator):
+        # spawn player inside guarenteed room for testing
         self.player = Player(6, 7)
-        self.rooms = room_generator.generate()
-        self.grid = self.make_grid(width, height)
+        test_room = Grid(Rectangle(5, 10, 5, 10), lambda:Ground())
 
-    def make_grid(self, width, height):
-        grid_rect = Rectangle(0, height, 0, width)
-        grid = Grid(grid_rect, lambda:Wall())
-        for room in self.rooms:
-            grid = grid.add_grids(room, room.rect.left, room.rect.top)
+        rooms = room_generator.generate()
+        rooms.append(test_room)
+        self.grid = self.make_grid(width, height, rooms)
+
+    def make_grid(self, width, height, rooms):
+        grid = Grid(Rectangle(0, height, 0, width), lambda:Wall())
+        for room in rooms:
+            grid = grid.add_grids(room)
         return grid
-
 
     def update_player(self, addx, addy):
         if not self.collide(addx, addy):
             self.player.update(addx, addy)
 
     def collide(self, addx, addy):
+        """remember, this only collides against self.grid, not all entities"""
         new_x = self.player.x + addx
         new_y = self.player.y + addy
         return self.grid.cells[new_y][new_x].collides()
